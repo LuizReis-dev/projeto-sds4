2cfd5113fb3f0f120bdfb4012fffaffbb2f62870
LuizReis-dev <reisluiz05@gmail.com>
2021-09-10 15:57:09 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'Utils/requests';
@@ -8,21 +9,22 @@ type ChartData = {
   series: number[];
 }
 const DonutChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-  //FORMA ERRADA
-  let chartData: ChartData = { labels: [], series: [] };
-
-  //FORMA ERRADA
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
     .then(res => {
       const data = res.data as SaleSum[];
       const myLabels = data.map(x => x.sellerName);
       const mySeries = data.map(x => x.sum);
 
-      chartData = { labels: myLabels, series: mySeries };
+      setChartData({ labels: myLabels, series: mySeries });
 
       console.log(chartData)
     });
+
+  }, [])
+
   //const mockData = {
   // series: [477138, 499928, 444867, 220426, 473088],
   //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
